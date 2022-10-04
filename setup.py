eff9ff15de85377b646a6aec72060e39b672042e
Anvar <anvar00755@mail.ru>
2022-10-04 23:53:32 +0300

    some fix

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Badmoon-mySun/cloudphoto",
     packages=setuptools.find_packages("src"),
+    include_package_data=True,
     package_dir={"": "src"},
     install_requires=all_reqs,
     classifiers=[
